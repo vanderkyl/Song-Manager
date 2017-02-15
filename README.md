a43b51b6464cf57f2ccb153d1a9e0682ec70b5ce
Kyle Vanderhoof <vanderhoof@gmail.com>
2017-02-15 01:57:53 -0600

    Minor fixes

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-# Song Manager
-Version # 0.4
+# Idea Manager
+Version # 0.5
 
-Features:
+New Features:
 - New file cache system in place
+- New styling for buttons and text
+- Auto login
+- State saving functionality
--- js/managers/disqus.js
+++ js/managers/disqus.js
@@ -1,27 +1,13 @@
 /**
 *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
 *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
-/*
-var disqus_config = function () {
-  this.page.url = "http://kylevanderhoof.com/";
-  this.page.identifier = "HOME_PAGE";
-  this.page.title = "Home Page";
-};
-
-(function() { // DON'T EDIT BELOW THIS LINE
-  var d = document, s = d.createElement('script');
-  s.src = 'http://idea-manager.disqus.com/embed.js';
-  s.setAttribute('data-timestamp', +new Date());
-  (d.head || d.body).appendChild(s);
-})();
-*/
 
 function loadDisqus(file) {
   DISQUS.reset({
       reload: true,
       config: function () {
           var id = file.id;
-          console.log(id);
+          console.log("Loading Disqus for this file: " + id);
           this.page.identifier = id;
           this.page.url = "http://kylevanderhoof.com/disqus?id=" + id;
           this.page.title = file.name;
